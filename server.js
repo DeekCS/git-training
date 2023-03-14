c94967715fdc1bc1aeab52ba5933f9ec20004289
Fadi Haddad <fadi.k.haddad@gmail.com>
2023-03-14 07:54:50 +0300

    Add power function

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/power", (req, res) => {
+    const {x, y} = req.query;
+    const power = x**y;
+    res.send(200, power);
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
